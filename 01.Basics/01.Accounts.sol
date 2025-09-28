e1266adabe54e61c1a493b23f10eda40c2bced77
sreekanthTa <sreekanthsreekantht970@gmail.com>
2025-09-28 09:32:33 +0530

    conveted diagrams

---
+++ 01.Basics/01.Accounts.sol
@@ -0,0 +1,15 @@
+// HOW CAN WE CONNECT TO THE ETHERIUM NETWORK ?
+// For developers => Web3.js
+// For Consumers => MetaMask
+
+// META MASK CONTAINS => 1. Account address => 0x98s4dg9s4f9gh84w9sg489sdfg4sdfg
+//                       2. Public Key  =>0xs9gd8dfb5df64gd4sdf6g84sd9g9d8f
+//                       3. Private Key => 0xbsg984sg984sf9d9df84fd65dg98
+ 
+//   1. Account Address => It is an user identifier like an email address
+//   2. Public Key + Private key => Act as a password for the account
+//   3. We should not share Private Keys since it is Private key, we shar eonly Public Key
+//   Note: We will use The One Account across all Etherium Network
+
+
+ 
